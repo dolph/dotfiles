4d84a487d560cdcb68ae61b4bcb46e757d8ef603
Dolph Mathews <dolph.mathews@gmail.com>
2013-11-01 11:18:56 -0500

    cleaned up dev deps

--- provision.sh
+++ provision.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # install homebrew and some python development tools
 ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
-brew install git python ack flake8 macvim
+brew install git python ack macvim
 pip install --upgrade setuptools
 pip install --upgrade pip
+pip install virtualenv virtualenvwrapper flake8
 
 # install dotfiles
 git clone https://github.com/dolph/dotfiles.git ~/dotfiles
