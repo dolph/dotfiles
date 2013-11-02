52a94568079c52b6db4e71f8783629f2b6a314dd
Dolph Mathews <dolph.mathews@gmail.com>
2013-11-03 01:24:19 +0800

    add ssh-copy-id

--- provision.sh
+++ provision.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # install homebrew and some python development tools
 ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
-brew install git python ack macvim tree
+brew install git python ack macvim tree ssh-copy-id
 pip install --upgrade setuptools
 pip install --upgrade pip
 pip install virtualenv virtualenvwrapper flake8
