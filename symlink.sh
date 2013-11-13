aee5cd89d911502461ba4fa4b92529cfa442f45b
Dolph Mathews <dolph.mathews@gmail.com>
2013-11-13 13:46:55 -0600

    delete existing files before symlinking

--- symlink.sh
+++ symlink.sh
@@ -9,10 +9,12 @@ rawfiles="bin"
 # symlink dotfiles to the the home dir, each prefixed by a dot (.)
 cd $dir
 for file in $dotfiles; do
+    rm -rf ~/.$file
     ln -s $dir/$file ~/.$file
 done
 
 # symlink regular files to the the home dir
 for file in $rawfiles; do
+    rm -rf ~/$file
     ln -s $dir/$file ~/$file
 done
