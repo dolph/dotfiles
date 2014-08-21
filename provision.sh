b51e1037e5d3212754ad5e44d5d2f351195964a3
Dolph Mathews <dolph.mathews@gmail.com>
2014-08-21 11:42:09 -0500

    add ansible to default brew packages

--- provision.sh
+++ provision.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # install homebrew and some python development tools
 ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
-brew install git python ack macvim tree ssh-copy-id
+brew install git python ack macvim tree ssh-copy-id ansible
 pip install --upgrade setuptools
 pip install --upgrade pip
 pip install virtualenv virtualenvwrapper flake8
