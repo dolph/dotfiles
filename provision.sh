718a530851efc7783c76c1611f04f67a98c09664
Dolph Mathews <dolph.mathews@gmail.com>
2015-01-28 15:36:37 -0600

    add support for cask apps

--- provision.sh
+++ provision.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 # install homebrew and some python development tools
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install git python ack macvim tree ssh-copy-id ansible
+brew install git
+brew install python
+brew install ack
+brew install macvim
+brew install tree
+brew install ssh-copy-id
+brew install ansible
+
+# install cask so we can install GUI apps
+brew install caskroom/cask/brew-cask
+brew install google-chrome
+brew install virtualbox
+brew install vagrant
+
 pip install --upgrade setuptools
 pip install --upgrade pip
-pip install virtualenv virtualenvwrapper hacking
+pip install virtualenv
+pip install virtualenvwrapper
+pip install hacking
 
 # install dotfiles
 git clone https://github.com/dolph/dotfiles.git ~/dotfiles
