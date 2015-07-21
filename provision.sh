89be56acff18346ace157980f465331afae613fe
Dolph Mathews <dolph.mathews@gmail.com>
2015-07-21 13:19:36 -0500

    sort brew deps

--- provision.sh
+++ provision.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # install homebrew and some python development tools
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install git
-brew install python
 brew install ack
-brew install macvim
-brew install tree
-brew install ssh-copy-id
 brew install ansible
+brew install git
+brew install macvim
+brew install python
+brew install ssh-copy-id
+brew install tree
 
 # install cask so we can install GUI apps
 brew install caskroom/cask/brew-cask
