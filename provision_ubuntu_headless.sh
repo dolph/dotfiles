6641a65a75302792f462d5e4345f126fef5189de
Dolph Mathews <dolph.mathews@gmail.com>
2016-07-18 17:31:01 +0000

    Add support for ubuntu server

---
+++ provision_ubuntu_headless.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+# Update packages.
+sudo apt update
+sudo apt upgrade -V -y
+
+# Install custom packages (git should already be installed).
+# google-chrome-stable \
+sudo apt install -V -y \
+    build-essential \
+    curl \
+    htop \
+    tmux \
+    tree \
+    vim \
+    ;
+
+sudo apt autoremove -y
+sudo apt clean
+sudo apt autoclean
+
+# Run updates.
+sh provision_common.sh
