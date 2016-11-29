e6c5be622bc2ba2b2281c608fff6c99d882228e1
Dolph Mathews <dolph.mathews@gmail.com>
2016-11-29 08:22:23 -0600

    Setup new ubuntu 16.04 desktop

--- provision_ubuntu_desktop.sh
+++ provision_ubuntu_desktop.sh
@@ -15,11 +15,11 @@ sudo apt upgrade -V -y
 # Install custom packages (git should already be installed).
 # google-chrome-stable \
 sudo apt install -V -y \
+    ack-grep \
     build-essential \
     curl \
     hexchat \
     htop \
-    pithos \
     python \
     tmux \
     tree \
@@ -38,13 +38,18 @@ sudo apt autoremove -y
 sudo apt clean
 sudo apt autoclean
 
+# Setup HexChat colors
+wget https://dl.hexchat.net/themes/Solarized%20Dark.hct /tmp/hexchat-solarized-dark.hct
+unzip -o /tmp/hexchat-solarized-dark.hct -d ~/.config/hexchat/
+rm /tmp/hexchat-solarized-dark.hct
+
 # Set terminal to Solarized Black color scheme.
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/palette '#000000000000:#DCDC32322F2F:#858599990000:#B5B589890000:#26268B8BD2D2:#D3D336368282:#2A2AA1A19898:#EEEEE8E8D5D5:#00002B2B3636:#CBCB4B4B1616:#58586E6E7575:#65657B7B8383:#838394949696:#6C6C7171C4C4:#9393A1A1A1A1:#FDFDF6F6E3E3'
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/bold_color '#9393a1a1a1a1'
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/background_color '#000000000000'
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/foreground_color '#838394949696'
-gconftool-2 -s -t bool /apps/gnome-terminal/profiles/Default/use_theme_colors false
-gconftool-2 -s -t bool /apps/gnome-terminal/profiles/Default/bold_color_same_as_fg false
+# gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/palette '#000000000000:#DCDC32322F2F:#858599990000:#B5B589890000:#26268B8BD2D2:#D3D336368282:#2A2AA1A19898:#EEEEE8E8D5D5:#00002B2B3636:#CBCB4B4B1616:#58586E6E7575:#65657B7B8383:#838394949696:#6C6C7171C4C4:#9393A1A1A1A1:#FDFDF6F6E3E3'
+# gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/bold_color '#9393a1a1a1a1'
+# gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/background_color '#000000000000'
+# gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/foreground_color '#838394949696'
+# gconftool-2 -s -t bool /apps/gnome-terminal/profiles/Default/use_theme_colors false
+# gconftool-2 -s -t bool /apps/gnome-terminal/profiles/Default/bold_color_same_as_fg false
 
 # Run updates.
 sh provision_common.sh
