b54eba454255af770ba22d1a301be9b48b020db8
Dolph Mathews <dolph.mathews@gmail.com>
2016-12-20 08:55:03 -0600

    Use scriptpath

--- provision_common.sh
+++ provision_common.sh
@@ -1,26 +1,28 @@
 #!/bin/sh
 set -e
 
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+
 # create symlinks from the home directory to dotfiles in ~/dotfiles
-dir=~/dotfiles
 dotfiles="ackrc bash_profile bashrc profile inputrc screenrc vim vimrc gvimrc gitignore_global tmux.conf"
 rawfiles="bin"
 
-# symlink dotfiles to the the home dir, each prefixed by a dot (.)
-cd $dir
+# symlink dotfiles to the home dir, each prefixed by a dot (.)
 for file in $dotfiles; do
     rm -rf ~/.$file
-    ln -s $dir/$file ~/.$file
+    ln -s $SCRIPTPATH/$file ~/.$file
 done
 
-# symlink regular files to the the home dir
+# symlink regular files to the home dir
 for file in $rawfiles; do
     rm -rf ~/$file
-    ln -s $dir/$file ~/$file
+    ln -s $SCRIPTPATH/$file ~/$file
 done
 
 # install vim plugins
-cd ~/dotfiles
 git submodule init
 git submodule update
 vim +PluginInstall! +PluginClean! +qall
