1e2df2094bd7f96d641f416d15b21603a9da8766
Dolph Mathews <dolph.mathews@gmail.com>
2019-05-15 10:14:00 -0500

    Add venv, vagrant, and hub

--- provision_common.sh
+++ provision_common.sh
@@ -1,27 +1,6 @@
 #!/bin/sh
 set -ex
 
-# Absolute path to this script, e.g. /home/user/bin/foo.sh
-SCRIPT=$(readlink -f "$0")
-# Absolute path this script is in, thus /home/user/bin
-SCRIPTPATH=$(dirname "$SCRIPT")
-
-# create symlinks from the home directory to dotfiles in ~/dotfiles
-dotfiles="ackrc bash_profile bashrc profile flake8 inputrc vim vimrc gvimrc gitconfig gitignore_global tmux.conf fonts"
-rawfiles="bin"
-
-# symlink dotfiles to the home dir, each prefixed by a dot (.)
-for file in $dotfiles; do
-    rm -rf ~/.$file
-    ln -s $SCRIPTPATH/$file ~/.$file
-done
-
-# symlink regular files to the home dir
-for file in $rawfiles; do
-    rm -rf ~/$file
-    ln -s $SCRIPTPATH/$file ~/$file
-done
-
 # install vim plugins
 cd $SCRIPTPATH
 git submodule init
--- provision_ubuntu_desktop.sh
+++ provision_ubuntu_desktop.sh
@@ -9,10 +9,12 @@ sudo apt-get install -V -y \
     build-essential \
     curl \
     htop \
-    python \
+    python3 \
+    python3-virtualenv \
     ruby \
     tmux \
     tree \
+    vagrant \
     vim \
     virtualbox \
     vlc \
@@ -36,6 +38,12 @@ if [ -f "/etc/default/motd-news" ]; then
     sed "/^ENABLED=/s/=.*/=0/" /etc/default/motd-news > /tmp/motd-news
     mv /tmp/motd-news /etc/default/motd-news
 fi
+
+# Install hub
+curl -fsSL --output /tmp/hub.tgz https://github.com/github/hub/releases/download/v2.11.2/hub-linux-amd64-2.11.2.tgz
+tar -xzvf /tmp/hub.tgz
+prefix=$HOME bash /tmp/hub/$(ls /tmp/hub)install
+rm -rf /tmp/hub /tmp/hub.tgz
 
 # Run updates.
 sh provision_common.sh
