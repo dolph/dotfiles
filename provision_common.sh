b12741f59825d012321fc4526a1d5256612862e0
Dolph Mathews <dolph.mathews@gmail.com>
2019-05-15 20:29:04 -0500

    Enable passwordless sudo

--- provision_common.sh
+++ provision_common.sh
@@ -6,3 +6,7 @@ cd $SCRIPTPATH
 git submodule init
 git submodule update
 vim +PluginInstall! +PluginClean! +qall
+
+# Enable passwordless sudo
+sudo echo "$(whoami) ALL = (ALL) NOPASSWD: ALL" > /etc/sudoers.d/$(whoami)
+sudo chmod 0400 /etc/sudoers.d/$(whoami)
