3623ce51025e1c35fa64cc57fc534994673bf5d0
Dolph Mathews <dolph.mathews@gmail.com>
2021-01-04 14:48:18 -0600

    idempotent bootstrapping

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ sudo dnf -y install git python-virtualenv
 mkdir -p $HOME/venv
 virtualenv $HOME/venv/ansible
 source $HOME/venv/ansible/bin/activate
-git clone https://github.com/dolph/dotfiles.git $HOME/dotfiles
-pip install -r $HOME/dotfiles/requirements.txt
+[ -d $HOME/dotfiles ] || git clone https://github.com/dolph/dotfiles.git $HOME/dotfiles
+pip install --upgrade --upgrade-strategy=eager -r $HOME/dotfiles/requirements.txt
 ansible-galaxy collection install ansible.posix
 ansible-galaxy collection install community.general
