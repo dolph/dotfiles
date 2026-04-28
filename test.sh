27e527f3975418dab378a7f0861ce3af57162398
Dolph Mathews <dolph.mathews@gmail.com>
2026-04-28 11:13:48 -0500

    Add github actions; update CI job

--- test.sh
+++ test.sh
@@ -2,20 +2,27 @@
 set -e
 
 ROLE=${1:-headless}
+FEDORA=${2:-$(curl -s 'https://bodhi.fedoraproject.org/releases/?state=current&rows_per_page=20' \
+    | python3 -c "
+import sys, json
+d = json.load(sys.stdin)
+releases = [r for r in d['releases'] if r['id_prefix'] == 'FEDORA' and not r['version'].startswith('E')]
+print(sorted(set(r['version'] for r in releases), key=int, reverse=True)[0])
+")}
 
-docker pull geerlingguy/docker-fedora31-ansible:latest
+docker pull geerlingguy/docker-fedora${FEDORA}-ansible:latest
 
-container_id=$(docker run --detach --privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro --volume="$(pwd):/etc/ansible:ro" geerlingguy/docker-fedora31-ansible:latest)
+container_id=$(docker run --detach --privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro --volume="$(pwd):/etc/ansible:ro" geerlingguy/docker-fedora${FEDORA}-ansible:latest)
 function cleanup {
     docker rm -f $container_id
 }
 trap cleanup EXIT
 
-docker exec --tty $container_id env TERM=xterm /etc/ansible/bootstrap.sh
+docker exec --tty "$container_id" env TERM=xterm /etc/ansible/bootstrap.sh
 
 if [ "$ROLE" == "headless" ]; then
-    docker exec --tty $container_id env TERM=xterm ansible-playbook /etc/ansible/headless.yml
+    docker exec --tty "$container_id" env TERM=xterm ansible-playbook /etc/ansible/headless.yml
 elif [ "$ROLE" == "desktop" ]; then
-    until docker exec --tty $container_id dnf groupinstall -y "Xfce Desktop"; do :; done
-    docker exec --tty $container_id env TERM=xterm ansible-playbook /etc/ansible/desktop.yml
+    until docker exec --tty "$container_id" dnf groupinstall -y "Xfce Desktop"; do :; done
+    docker exec --tty "$container_id" env TERM=xterm ansible-playbook /etc/ansible/desktop.yml
 fi
